c0fdfddf88e70ef6d5c6de4b4800cecd6a0429f7
Bal48ol <day.of.travel69@gmail.com>
2023-07-27 20:47:16 +0300

    добавлен вход по отпечатку пальца

--- app/src/main/java/com/example/meetupsync/PinCodeActivity.java
+++ app/src/main/java/com/example/meetupsync/PinCodeActivity.java
@@ -2,17 +2,28 @@ package com.example.meetupsync;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.ConsumerIrManager;
+import android.hardware.biometrics.BiometricManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
+import java.util.concurrent.Executor;
 
 public class PinCodeActivity extends AppCompatActivity {
 
@@ -22,25 +33,57 @@ public class PinCodeActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
     private boolean isFirstRun;
 
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
+    ConstraintLayout mMainLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin_code);
+
 
         // Открытие клавиатуры сразу при открытии приложения
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
         pinCodeEditText = findViewById(R.id.pinCodeEditText);
         submitButton = findViewById(R.id.submitButton);
+        mMainLayout = findViewById(R.id.main_layout);
+        ImageView fingerImageView = findViewById(R.id.fingerImageView);
+
+
+        androidx.biometric.BiometricManager biometricManager = androidx.biometric.BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Log.d("MY_APP_TAG", "Устройство поддерживает вход по отпечатку пальца.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Log.e("MY_APP_TAG", "Устройство не поддерживает вход по отпечатку пальца.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Log.e("MY_APP_TAG", "Проверка отпечатка пальца не работает");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Log.e("MY_APP_TAG", "Отпечаток пальца не установлен");
+                break;
+        }
+        fingerImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBiometricPrompt();
+            }
+        });
 
         sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
         isFirstRun = sharedPreferences.getBoolean("isFirstRun", true);
 
-
         if (isFirstRun) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
             submitButton.setText("СОХРАНИТЬ");
+            fingerImageView.setVisibility(View.GONE);
         } else {
             submitButton.setText("ВОЙТИ");
+            showBiometricPrompt();
         }
 
         // Установка курсора в поле для ввода пароля
@@ -60,6 +103,7 @@ public class PinCodeActivity extends AppCompatActivity {
 
                     // Переход к следующей странице
                     Intent intent = new Intent(PinCodeActivity.this, MainActivity.class);
+                    Toast.makeText(getApplicationContext(), "Пин-код сохранён", Toast.LENGTH_SHORT).show();
                     startActivity(intent);
                     finish();
                 } else {
@@ -68,6 +112,7 @@ public class PinCodeActivity extends AppCompatActivity {
                     if (pinCode.equals(savedPinCode)) {
                         // Пин-код верный, переход к следующей странице
                         Intent intent = new Intent(PinCodeActivity.this, MainActivity.class);
+                        Toast.makeText(getApplicationContext(), "Вход выполнен", Toast.LENGTH_SHORT).show();
                         startActivity(intent);
                         finish();
                     } else {
@@ -117,7 +162,38 @@ public class PinCodeActivity extends AppCompatActivity {
                 return false;
             }
         });
-
     }
 
+    private void showBiometricPrompt() {
+        Executor executor = ContextCompat.getMainExecutor(this);
+        BiometricPrompt biometricPrompt = new BiometricPrompt(PinCodeActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(), "Вход выполнен", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(PinCodeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                // Обработка ошибки аутентификации
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                // Обработка неудачной аутентификации
+            }
+        });
+
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Используйте отпечаток пальца")
+                .setNegativeButtonText("Использовать пин-код")
+                .build();
+
+        biometricPrompt.authenticate(promptInfo);
+    }
 }
